a2998c9bc7d7a7111670a1aeb76fdf7f57f765e6
啊茶kk <acgr18share@gmail.com>
2025-01-13 18:17:44 +0800

    更改根目录下的 astro.config.mjs 文件，24行site: "https://pilipiala.pages.dev"

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari-32d.pages.dev/",
+  site: "https://pilipiala.pages.dev",
   base: "/",
   trailingSlash: "always",
   integrations: [
